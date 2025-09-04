44725088d8f363bea0101599bcb25088b4b2099e
joaquintoranzo10 <joaquintoranzocontac@gmail.com>
2025-09-04 20:05:47 -0300

    para listar y crear ususarios
    para operacioes sobre un usuario especifico

--- crminm/urls.py
+++ crminm/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from usuarios.views import ListaYCreaUsuario, DetalleUsuario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Para listar y crear usuarios
+    path('api/usuarios/', ListaYCreaUsuario.as_view(), name='usuarios-lista'),
+    
+    # Para operaciones sobre un usuario específico
+    path('api/usuarios/<int:pk>/', DetalleUsuario.as_view(), name='usuario-detalle'),
 ]
+
